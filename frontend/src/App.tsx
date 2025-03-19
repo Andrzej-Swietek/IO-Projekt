65a543af1cc8e97f82fb9a6e4b1bd42d67dd71d5
Andrzej Świętek <mr.andi321@gmail.com>
2025-03-19 21:25:24 +0100

    Initialized Repo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,43 @@
+import { FC, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+interface Props {}
+
+const App: FC<Props> = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card shadow">
+        <button onClick={() => setCount(currentCount => currentCount + 1)}>
+          count is
+          {' '}
+          {count}
+        </button>
+        <p>
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+};
+
+export default App;
---
+++ io-project/src/test/java/pl/edu/agh/io_project/IoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.io_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
